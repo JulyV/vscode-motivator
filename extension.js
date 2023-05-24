600ec955bd82fc1057234cde4ea506598dd35885
Julia Sugak <juliasugak@Air-July.fritz.box>
2023-05-24 23:35:52 +0200

    add new pharases

--- extension.js
+++ extension.js
@@ -1,24 +1,20 @@
-'use strict';
-const 
-    vscode = require('vscode'),
-    generateMessage = require('./motivationMessages');
+"use strict";
+const vscode = require("vscode");
+const { generateMessage } = require("./motivationMessages");
 
 function activate(context) {
-    var disposable = vscode.workspace.onDidSaveTextDocument(() => {
-        if (!this._statusBarItem) {
-            this._statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
-            this._statusBarItem.text = generateMessage();
-        } else {
-            this._statusBarItem.text = generateMessage();     
-        }
-        return this._statusBarItem.show();
-    });
+  const disposable = vscode.workspace.onDidSaveTextDocument(() => {
+    if (!this._statusBarItem) {
+      this._statusBarItem = vscode.window.createStatusBarItem(
+        vscode.StatusBarAlignment.Left
+      );
+      this._statusBarItem.text = generateMessage();
+    } else {
+      this._statusBarItem.text = generateMessage();
+    }
+    return this._statusBarItem.show();
+  });
 
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 exports.activate = activate;
-
-function deactivate() {
-}
-
-exports.deactivate = deactivate;
